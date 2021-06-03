9ea3c0b784885b45d08c6bef29a78343dc7e6aba
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-06-03 12:50:57 -0300

    base

---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+
+class Server {
+
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT
+        this.middlewares()
+        this.routes()
+        
+        
+    }
+
+
+    middlewares(){
+
+        this.app.use( cors() )
+        this.app.use( express.json() )
+    }
+
+    routes(){
+        this.app.get('/', (req, res)=>{
+            res.json({
+                msg: 'Hola Ramita'
+            })
+        })
+    }
+
+
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log('Servidor corriendo en el puerto ',this.port)
+        })
+    }
+
+}
+
+module.exports = Server
